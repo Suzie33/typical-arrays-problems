d924a2ade0f7b498aff4113e1ef62d5681f148bb
Valentina Ponamareva <valiazin@gmail.com>
2020-09-30 11:47:41 +0300

    feat: implement min function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  let minValue = 0;
+
+  for (let item of array) {
+    if (item < minValue) {
+      minValue = item;
+    }
+  }
+  return minValue;
 }
 
 exports.max = function max (array) {
